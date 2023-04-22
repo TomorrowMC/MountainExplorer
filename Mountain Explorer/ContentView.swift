e58dda6c31d3d652c1ace5d0fa2997f31a13a5ac
Yifei.Hu <normalmoc@icloud.com>
2023-04-22 16:16:07 +0800

    Initial Commit

---
+++ Mountain Explorer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Mountain Explorer
+//
+//  Created by 胡逸飞 on 2023/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mountain Explorer/Mountain_ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mountain_ExplorerApp.swift
+//  Mountain Explorer
+//
+//  Created by 胡逸飞 on 2023/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Mountain_ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
